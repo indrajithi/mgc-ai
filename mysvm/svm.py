4f42caed0675545f82af79fb544838631128b31e
indrajithi <i.indrajith.143@gmail.com>
2017-06-02 13:01:03 +0530

    removed commented condes and fixed doc strings

--- mysvm/svm.py
+++ mysvm/svm.py
@@ -30,68 +30,25 @@ clf_path = pkg_resources.resource_filename(resource_package, resource_path)
 clfprob = joblib.load(clf_path)
 
 label = feature.getlabels()
-"""
-def svms():
-    #linear kernel=======================================================
-    print('='*5+' Linear Kernel SVM '+'='*5)
-    clf = svm.LinearSVC(C=10000)
-    clf.fit(X90,Y90)
 
-    #cross validation
-    resub = clf.predict(X90)
-    resubAcc = acc.get(resub,Y90)
-    #test case
-    print('Resubstituion Accuracy : ',resubAcc)
-    resTest = clf.predict(xt10)
-    tesAcc = acc.get(resTest,yt10)
-    print('Test case accuracy : ',tesAcc)
-    scores = cross_val_score(clf, X90, Y90, cv=5)
-    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    print('='*30)
-
-    #Polynomial kernel=======================================================
-    print('='*5+' Polynomial Kernel SVM '+'='*5)
-    clf = svm.SVC(kernel='poly',C=1000)
-    clf.fit(X,Y)
-
-    #cross validation
-    resub = clf.predict(X)
-    resubAcc = acc.get(resub,Y)
-    #test case
-    print('Resubstituion Accuracy : ',resubAcc)
-    resTest = clf.predict(xt10)
-    tesAcc = acc.get(resTest,yt10)
-    print('Test case accuracy : ',tesAcc)
-
-    scores = cross_val_score(clf, xt, yt, cv=5)
-    print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    print('='*30)
-
-"""
 
 def poly(X,Y):
+    """
+    Returns a polynomial kernal svm
+    """
     #Polynomial kernel=======================================================
     clf = svm.SVC(kernel='poly',C=1)
     clf.fit(X,Y)
-    #cross validation
-    #resub = clf.predict(X)
-    #resubAcc = acc.get(resub,Y)
-    #test case
-    #resTest = clf.predict(xt)
-    #tesAcc = acc.get(resTest,yt)
-    #scores = cross_val_score(clf, X, Y, cv=5)
 
-    #return scores.mean()
     return clf
 
 
 def fit(train_percentage,fold=5):
-    """ Accepts parameter: 
-            train_percentage;
-            fold;
+    """ Radomly choose songs from the dataset, and train the classfier 
+        Accepts parameter: train_percentage, fold;
+        Returns clf    
     """
-    #creates a matrix of size 10x100x104
-    #return clf
+    
     resTrain =0
     resTest = 0
     score = 0
@@ -139,6 +96,9 @@ def fit(train_percentage,fold=5):
 
 
 def getprob(filename):
+    """
+    Find the probality that a song belongs to each genre. 
+    """
     x = feature.extract(filename)
     clf = clfprob
     prob = clf.predict_proba(x)[0]
@@ -146,13 +106,6 @@ def getprob(filename):
     # max probablity 
     m = max(dd,key=dd.get)
     print(m, dd[m])
-    
-    """ to print all probablity and difference from max
-    for i in dd:
-        if i != m:
-            if dd[m] - dd[i] :
-                print(i, dd[i], '| diff:',dd[m] - dd[i])
-    """
 
     sorted_genre = sorted(dd,key=dd.get,reverse=True)
     has_features_of = []
@@ -167,11 +120,10 @@ def getprob(filename):
 
 
 def random_cross_validation(train_percentage,fold):
-    """ Accepts parameter: 
-            train_percentage;
-            fold;
+    """ 
+    Randomly crossvalidate with training percentage and fold. Accepts parameter: train_percentage, fold;
     """
-    #creates a matrix of size 10x100x104
+
     resTrain =0
     resTest = 0
     score = 0
@@ -210,37 +162,18 @@ def random_cross_validation(train_percentage,fold):
                 else:
                     test_matrix = np.vstack([test_matrix, Xall[stack[test_counter]]])
 
-        #print(train_matrix.shape)
-        #print(test_matrix.shape)
-
         Y = feature.geny(train_percentage) 
         y = feature.geny(100 - train_percentage)
         #training accuracy
         clf = svm.SVC(kernel='poly',C=1)
         clf.fit(train_matrix, Y)
-        #train case
-        #scores = cross_val_score(clf, train_matrix, feature.geny(train_percentage), cv=5)
-        #print("acc train",scores.mean())
-        #test case
-        #scores = cross_val_score(clf, test_matrix, feature.geny(100 - train_percentage), cv=5)
-        #print("acc test",scores.mean())
+ 
         res = clf.predict(train_matrix)
         #print(acc.get(res,Y))
         resTrain += acc.get(res,Y)
         res = clf.predict(test_matrix)
         resTest += acc.get(res,y)
-        #print(acc.get(res,y))
-        #return test_matrix, train_matrix
-        #scores = cross_val_score(clf, test_matrix, y, cv=5)
-        #print("Cross Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-        
-        #score = 100 *clf.score(test_matrix,y)
-        #print(score)
-        #scores += score
-
-    #print(scores/fold)
-    #print(resTrain)
-    #print(resTest)
+    
     print("Training accuracy with %d fold %f: " % (int(fold), resTrain / int(fold)))
     print("Testing accuracy with %d fold %f: " % (int(fold), resTest / int(fold)))
     
@@ -248,7 +181,8 @@ def random_cross_validation(train_percentage,fold):
 
 def findsubclass(class_count):
     """ 
-    Finds the combination of classes NCR
+    Returns all possible ways we can combine the classes. 
+    Accepts an integer as class count
     """
     class_l = list(range(10))
     flag = True
@@ -262,6 +196,9 @@ def findsubclass(class_count):
     return labels
 
 def gen_sub_data(class_l):
+    """
+    Generate a subset of the dataset for the given list of classes
+    """
     all_x = np.array([])
     flag = True;
 
@@ -278,11 +215,13 @@ def gen_sub_data(class_l):
 
 
 def fitsvm(Xall,Yall,class_l,train_percentage,fold):
-    """ Accepts parameter: 
+    """ 
+    Fits an poly svm and returns the accuracy
+    Accepts parameter: 
             train_percentage;
             fold;
+    Returns: classifier, Accuracy
     """
-    #creates a matrix of size 10x100x104
     resTrain =0
     resTest = 0
     score = 0
@@ -335,6 +274,11 @@ def fitsvm(Xall,Yall,class_l,train_percentage,fold):
     return clf , resTest / int(fold)
 
 def best_combinations(class_l, train_percentage, fold):
+    """
+    Finds all possible combination of classes and the accuracy for the given number of classes
+    Accepts: Training percentage, and number of folds
+    Returns: A List of best combination possible for given the class count.
+    """
     class_comb = findsubclass(class_l)
     avg = []
     X = gen_sub_data(class_comb[0])
